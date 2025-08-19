d1e2cddb5f9c5e6d04797df7edb03970e5ac880e
ShreevathsaR <shreevathsar2002@gmail.com>
2025-08-19 17:53:27 +0530

    fix:deployment fixes

--- app-frontend/src/lib/api.tsx
+++ app-frontend/src/lib/api.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://news-chatbot-j7rc.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://news-chatbot-j7rc.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
 });
 
 api.interceptors.request.use(
